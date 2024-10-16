fef73d4fb183a1668522859001a10853d6558450
Sabrina-Sawyer <sabrinagtc242@gmail.com>
2024-10-16 16:56:08 -0400

    added models

---
+++ server/src/models/index.ts
@@ -0,0 +1,4 @@
+// compile all the models into this index using imports and export default all of them
+import User from './User';
+
+export default { User };
---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,3 @@
+import 
+
+// todo: create resolvers for the Query and Mutation of data, export default resolvers
