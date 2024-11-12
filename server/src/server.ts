47a0d4348ad9f36e7ec6ce04bca89b4d25f01781
Sabrina-Sawyer <sabrinagtc242@gmail.com>
2024-11-12 18:27:13 -0500

    added more dots

--- server/src/server.ts
+++ server/src/server.ts
@@ -47,10 +47,10 @@ const startServer = async () => {
     
     // Serve static assets in production
     if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/dist')));
+        app.use(express.static(path.join(__dirname, '../../client/dist')));
 
         app.get('*', (_req, res) => {
-            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+            res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
         });
     }
 
